cc4505598f71a83b42be8e6b075136de65d9cafe
ZhouSusan <szhou089@gmail.com>
2022-11-09 06:00:08 -0800

    created duck object and called name() and sound() in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Duck donaldDuck = new Duck();
+        System.out.format("Donald %s says %s", donaldDuck.name(), donaldDuck.sound());
     }
 }
